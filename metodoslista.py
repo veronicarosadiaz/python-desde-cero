38f4a49b6de9d6e90a261f90340f5232f03b86aa
veronicarosadiaz <127392943+veronicarosadiaz@users.noreply.github.com>
2023-12-04 19:39:30 +0000

    Add files via upload

---
+++ metodoslista.py
@@ -0,0 +1,26 @@
+lista = [1,2,3]
+#añadir un elementoa la lista
+lista.append()
+#añadir el contenido de una lista a otra
+lista2=[4,5,6]
+lista.extend(lista2)
+#añadir un elemento en una posición determinada de la lista
+lista.insert(1,100)
+#quitar el último elemento de la lista y mostrarlo
+lista.pop()
+#eliminar un valor de la lista
+lista.remove(3)
+#invertir la lista
+lista.reverse()
+#ordenar la lista
+lista.sort()
+#ordenar la lista al revés
+lista.sort(reverse=True)
+#lista de cadena de caracteres
+lista3=["hola", "que", "tal"]
+#cuántas apariciones tiene un elemento?:
+lista.count(5)
+#en qué posición está la primera aparición de un elemento?:
+lista.index(3)
+#buscar un elemento a partir de una posición (el 3 a partir de la posición 1, por ejempo):
+lista.index(3,1)
